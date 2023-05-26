6d9615b2eaac1ffdae0b9206060fbdecbbcade8d
TylerDuffie1 <116038387+TylerDuffie1@users.noreply.github.com>
2023-05-26 10:48:21 -0400

    Update script.js
    Added comments to the script.js file to help better understand the code.

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-const audioHit = new Audio('../assets/15.mp3')
-const song = new Audio('../assets/15.mp3')
+// Initialize audio objects with audio file paths
+const audioHit = new Audio('../assets/15.mp3'); // Sound effect audio
+const song = new Audio('../assets/15.mp3'); // Background music audio
 
+/**
+
+Function to play an audio object
+@param {object} audioObject - The audio object to play
+*/
 function playAudio(audioObject) {
-  audioObject.play();
+audioObject.play();
 }
+/**
 
+Function to loop an audio object
+@param {object} audioObject - The audio object to loop
+*/
 function loopAudio(audioObject) {
-  audioObject.loop = true;
-  playAudio(audioObject);
+audioObject.loop = true;
+playAudio(audioObject);
 }
+/**
 
+Function to stop an audio object
+@param {object} audioObject - The audio object to stop
+*/
 function stopAudio(audioObject) {
-  audioObject.pause();
+audioObject.pause();
 }
+/**
 
-function play(){
-  playAudio(song);
+Function to play the background music
+*/
+function play() {
+playAudio(song);
 }
